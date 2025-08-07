cfce6d99cabf817a2571ea6abcd1d73b123a12d4
David.Koeller <David.Koeller@volteuropa.org>
2025-08-07 11:53:18 +0200

    update to something useful

--- app/main.py
+++ app/main.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, request, redirect, url_for
 import os
-import random as rs
+import random
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'app/static'
+ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file"
+        if file and allowed_file(file.filename):
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return redirect(url_for('gallery'))
     return render_template('upload.html')
 
 @app.route('/gallery')
 def gallery():
-    random_nr = rs.randint(0, 4)
-    img_to_show = f'img{random_nr}.jpeg'
-    
-    return render_template('gallery.html', img_to_show=img_to_show)
+    image_files = [f for f in os.listdir(UPLOAD_FOLDER) if f.lower().endswith(('jpg', 'jpeg', 'png'))]
+    random.shuffle(image_files)
+    return render_template('gallery.html', images=image_files)
 
 if __name__ == '__main__':
     app.run(debug=True)
